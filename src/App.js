30632ba13a44f46c55ae55326760b383404622a1
Henry <henryhnguyen98@gmail.com>
2020-09-23 22:03:02 -0700

    colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import 'weather-icons/css/weather-icons.css';
@@ -16,13 +16,13 @@ class App extends React.Component{
       country: undefined,
       icon: undefined,
       main: undefined,
-      celsius: undefined,
+      fahrenheit: undefined,
       temp_max: undefined,
       temp_min: undefined,
       description: "",
-      error: false
+      error: false,
+      color: '#ffcce6'
     };
-    this.getWeather();
 
     this.weatherIcon = {
       Thunderstorm: "wi-thunderstorm",
@@ -35,64 +35,94 @@ class App extends React.Component{
     };
   }
 
-  calCelsius(temp){
-    let cell = Math.floor(temp - 273.15);
-    return cell;
+  calFahrenheit(temp){
+    let fahr = Math.floor(((temp - 273.15) * 1.8) + 32);
+    return fahr;
   }
 
   get_WeatherIcon(icons, rangeId) {
     switch (true) {
       case rangeId >= 200 && rangeId < 232:
         this.setState({ icon: icons.Thunderstorm });
+        this.setState({ color: '#ffffcc'});
         break;
       case rangeId >= 300 && rangeId <= 321:
         this.setState({ icon: icons.Drizzle });
+        this.setState({ color: '#ccffff'});
         break;
       case rangeId >= 500 && rangeId <= 521:
         this.setState({ icon: icons.Rain });
+        this.setState({ color: '#ccd9ff'});
         break;
       case rangeId >= 600 && rangeId <= 622:
         this.setState({ icon: icons.Snow });
+        this.setState({ color: '#ccccff'});
         break;
       case rangeId >= 701 && rangeId <= 781:
         this.setState({ icon: icons.Atmosphere });
+        this.setState({ color: '#ffcccc'});
         break;
       case rangeId === 800:
         this.setState({ icon: icons.Clear });
+        this.setState({ color: '#ccffcc'});
         break;
       case rangeId >= 801 && rangeId <= 804:
         this.setState({ icon: icons.Clouds });
+        this.setState({ color: '#ccffe6'});
         break;
       default:
         this.setState({ icon: icons.Clouds });
+        this.setState({ color: '#ffcce6'});
     }
   }
 
-  getWeather = async () =>{
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+  getWeather = async (e) => {
+    e.preventDefault();
 
-    const response = await api_call.json();
+    const country = e.target.elements.country.value;
+    const city = e.target.elements.city.value;
 
-    console.log(response);
 
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      celsius: this.calCelsius(response.main.temp),
-      temp_max: this.calCelsius(response.main.temp_max),
-      temp_min: this.calCelsius(response.main.temp_min),
-      description: response.weather[0].description,
-      
-    });
-    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+
+      const response = await api_call.json();
+      console.log(response)
+
+      if (country && city && response.cod !== "404") {
+
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        country: response.sys.country,
+        main: response.weather[0].main,
+        fahrenheit: this.calFahrenheit(response.main.temp),
+        temp_max: this.calFahrenheit(response.main.temp_max),
+        temp_min: this.calFahrenheit(response.main.temp_min),
+        description: response.weather[0].description,
+        error: false
+      });
+
+      // setting icons
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+      document.body.style.background = "white";
+
+      console.log(response);
+    } else {
+      this.setState({
+        error: true
+      });
+    }
   };
 
   render(){
+    const background_type = {
+      background: this.state.color
+    };
     return(
-      <div className="App">
-        <Form/>
+      <div style = {background_type} className="App">
+        <Form loadweather={this.getWeather} error={this.state.error}/>
         <Weather city = {this.state.city} country = {this.state.country} 
-        temp_celcius = {this.state.celsius} temp_max = {this.state.temp_max}
+        temp_fahrenheit = {this.state.fahrenheit} temp_max = {this.state.temp_max}
         temp_min = {this.state.temp_min} description= {this.state.description}
         weatherIcon = {this.state.icon}/>
       </div>
--- src/components/form.component.jsx
+++ src/components/form.component.jsx
@@ -4,7 +4,9 @@ import "./form.style.css"
 const Form = props => {
     return (
         <div className = "container">
-            <form>
+            <br></br>
+            <div>{props.error ? error() : null}</div>
+            <form onSubmit={props.loadweather}>
                 <div className = "row">
                     <div className = "col-md-3 offset-md-2">
                         <input 
@@ -23,12 +25,20 @@ const Form = props => {
                         placeholder ="Country"/>
                     </div>
                     <div className = "col-md-3 mt-md-0 text-md-left">
-                        <button className = "btn btn-warning"> Get Weather </button> 
+                        <button className = "btn btn-warning"> Check Weather </button> 
                     </div>
                 </div>
             </form>
         </div>
-    )
+    );
+};
+
+function error(){
+    return(
+        <div className="alert alert-danger mx-5" role = "alert">
+            Please Enter a Valid City and Country
+        </div>
+    );
 }
 
 export default Form;
